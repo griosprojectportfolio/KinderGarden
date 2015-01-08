e7bd70bf79f6fc66d653c67c103418f0848e325a
Ajay singh <ajay@grepruby.com>
2015-01-08 12:24:43 +0530

    Initial Commit

---
+++ Kindergarten/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kindergarten
+//
+//  Created by GrepRuby on 08/01/15.
+//  Copyright (c) 2015 GrepRuby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
